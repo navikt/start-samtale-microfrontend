5428d844507fdcb3447957fbfa7cd82421d72191
johannetronstad <johanne.tronstad@nav.no>
2025-02-25 08:29:15 +0100

    Legg til nais fil og oppdater deploy fil

--- src/utils/amplitude.ts
+++ src/utils/amplitude.ts
@@ -4,4 +4,4 @@ const logger = getAmplitudeInstance("dekoratoren");
 
 //TODO: komponent må fylles ut med et identifiserende navn for microfrontend
 //Default er at navnet på appen blir benyttet ved replace av "tms-microfrontend-template"
-export const logNavigereEvent = () => logger("navigere", { komponent: "tms-microfrontend-template" });
+export const logNavigereEvent = () => logger("navigere", { komponent: "start-samtale-microfrontend" });
