25da5fae709fbf3603e64aa5f1ada7f18ff18cc3
johannetronstad <johanne.tronstad@nav.no>
2025-02-25 14:31:26 +0100

    Fiks tester

--- src/components/Komponent.test.tsx
+++ src/components/Komponent.test.tsx
@@ -2,9 +2,14 @@ import { describe, expect, it } from "vitest";
 import { render, screen } from "../utils/test-utils";
 import Komponent from "./Komponent";
 
-describe("Simple working test for Komponent", () => {
-  it("has a text", () => {
-    render(<Komponent tekst="tekst" />);
-    expect(screen.getByText(/tekst/)).toBeDefined();
+describe("Microfrontend", () => {
+  it("should have a headline", () => {
+    render(<Komponent />);
+    expect(screen.getByText("Du kan få veiledning")).toBeDefined();
+  });
+
+  it("should have a description", () => {
+    render(<Komponent />);
+    expect(screen.getByText("Vi ønsker å bli bedre kjent med deg og situasjonen din, slik at du kan få veiledning som passer for deg.")).toBeDefined();
   });
 });
