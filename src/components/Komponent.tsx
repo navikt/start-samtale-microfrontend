b9756b3d83a2ac03e97536901035b1eac06818f8
johannetronstad <johanne.tronstad@nav.no>
2025-02-26 12:51:43 +0100

    Trigg bygg

--- src/components/Komponent.tsx
+++ src/components/Komponent.tsx
@@ -10,17 +10,17 @@ const Komponent = () => {
   const language = useContext(LanguageContext);
   return (
     <Link href={startSamtaleLenke} variant="neutral" className={styles.link}>
-    <div className={styles.container}>
-      <div className={styles.header}>
-        <Heading size="small" className={styles.heading}>
-          {text.card.heading[language]}
-        </Heading>
-        <ChevronRightIcon className={styles.chevron} aria-hidden fontSize="24px" />
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <Heading size="small" className={styles.heading}>
+            {text.card.heading[language]}
+          </Heading>
+          <ChevronRightIcon className={styles.chevron} aria-hidden fontSize="24px" />
+        </div>
+        <div className={styles.body}>
+          <BodyShort className={styles.detail}>{text.card.description[language]}</BodyShort>
+        </div>
       </div>
-      <div className={styles.body}>
-        <BodyShort className={styles.detail}>{text.card.description[language]}</BodyShort>
-      </div>
-    </div>
     </Link>
   );
 };
